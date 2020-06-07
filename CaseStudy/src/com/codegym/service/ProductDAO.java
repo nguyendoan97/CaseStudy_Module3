7e20060f7ee3b217441a939f4cccd75766e73865
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-06-07 12:19:24 +0700

    add final sql

--- CaseStudy/src/com/codegym/service/ProductDAO.java
+++ CaseStudy/src/com/codegym/service/ProductDAO.java
@@ -1,23 +1,23 @@
-//package com.codegym.service;
-//
-//import com.codegym.model.Product;
-//
-//import java.sql.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class ProductDAO {
-//    private String jdbcURL="jdbc:mysql://localhost:3306/demo_user?useUnicode=yes&characterEncoding=UTF-8";
-//    private String jdbcUsername= "root";
-//    private String jdbcPassword= "123456";
-//
-//    private static final String INSERT_USERS_SQL="INSERT INTO users"+"(name,email,country)VALUES"+"(?,?,?);";
-//    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id=?;";
-//    private static final String SELECT_USER_BY_COUNTRY = "select id,name,email,country from users where country=?;";
-//    private static final String SELECT_ALL_USERS = "select * from users";
-//    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
-//    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
-//
+package com.codegym.service;
+
+import com.codegym.model.Product;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+    private String jdbcURL="jdbc:mysql://localhost:3306/demo_user?useUnicode=yes&characterEncoding=UTF-8";
+    private String jdbcUsername= "root";
+    private String jdbcPassword= "123456";
+
+    private static final String INSERT_USERS_SQL="INSERT INTO users"+"(name,email,country)VALUES"+"(?,?,?);";
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id=?;";
+    private static final String SELECT_USER_BY_COUNTRY = "select id,name,email,country from users where country=?;";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+
 //    public ProductDAO() {
 //    }
 //
