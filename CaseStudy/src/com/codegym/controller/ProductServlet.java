c8505ace2cd5fc63bf245f78a441b74c83f48f55
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-06-07 12:37:48 +0700

    cap nhat servlet

--- CaseStudy/src/com/codegym/controller/ProductServlet.java
+++ CaseStudy/src/com/codegym/controller/ProductServlet.java
@@ -1,28 +1,28 @@
-//package com.codegym.controller;
-//
-//import com.codegym.service.ProductDAO;
-//import com.codegym.model.Product;
-//
-//import java.io.IOException;
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@WebServlet(name = "UserServlet", urlPatterns = "/products")
-//public class ProductServlet extends HttpServlet {
-//    private static final long serialVersionUID = 1L;
-//    private ProductDAO productDAO;
-//
-//    public void init() {
-//        productDAO = new ProductDAO();
-//    }
-//
+package com.codegym.controller;
+
+import com.codegym.service.ProductDAO;
+import com.codegym.model.Product;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "UserServlet", urlPatterns = "/products")
+public class ProductServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private ProductDAO productDAO;
+
+    public void init() {
+        productDAO = new ProductDAO();
+    }
+
 //    protected void doPost(HttpServletRequest request, HttpServletResponse response)
 //            throws ServletException, IOException {
 //        String action = request.getParameter("action");
