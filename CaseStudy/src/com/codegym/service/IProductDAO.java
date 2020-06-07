66c868adbac91de25ec22c090992fdebece24f81
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-06-07 12:17:48 +0700

    tao interface DAO

--- CaseStudy/src/com/codegym/service/IProductDAO.java
+++ CaseStudy/src/com/codegym/service/IProductDAO.java
@@ -1,18 +1,18 @@
-//package com.codegym.service;
-//
-//import com.codegym.model.Product;
-//
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//public interface IProductDAO {
-//    public void insertUser(Product product) throws SQLException;
-//
-//    public Product selectUser(int id);
-//
-//    public List<Product> selectAllUsers();
-//
-//    public boolean deleteUser(int id) throws SQLException;
-//
-//    public boolean updateUser(Product product) throws SQLException;
-//}
+package com.codegym.service;
+
+import com.codegym.model.Product;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IProductDAO {
+    public void insertUser(Product product) throws SQLException;
+
+    public Product selectUser(int id);
+
+    public List<Product> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(Product product) throws SQLException;
+}
